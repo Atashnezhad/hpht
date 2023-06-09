a7db64f9018772618dde39e2791ea3566cc258fc
atashnezhad <atashnezhad1@gmail.com>
2023-06-08 23:04:54 -0500

    make a class prac

--- practice.py
+++ practice.py
@@ -3,8 +3,13 @@ import json
 from pathlib import Path
 from typing import List, Dict
 import logging
+
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 from pydantic import BaseModel, Field, validator
-from collections import defaultdict
+import itertools
+
 # set up logging for info and above
 logging.basicConfig(level=logging.CRITICAL)
 
@@ -37,6 +42,50 @@ class Data(BaseModel):
     DES117_CP: List[SUBDATA]
 
 
+class MyWork:
+    def __init__(self, *args, **kwargs):
+        self.data = None
+
+    def make_heatmap(self, my_dict, ILs, temperature, pressure):
+        df = pd.DataFrame(my_dict[f"{ILs}_t_{temperature}_p_{pressure}"])
+        # Create a pivot table to rearrange the data
+        df_pivot = df.pivot_table(values="cP", index="SS", columns="SR")
+        # Create the heat map
+        try:
+            sns.heatmap(df, cmap="coolwarm", annot=True,
+                        fmt=".2f", cbar_kws={"label": "cP"}
+                        )
+            plt.xlabel("SR")
+            plt.ylabel("SS")
+            plt.title("Heat Map: SS vs SR with cP")
+            plt.show()
+        except ValueError:
+            print(f"No data for {ILs}_t_{temperature}_p_{pressure}")
+
+    def method_1(self):
+        temperatures = [74, 120, 220, 310, 410, 480]
+        pressures = [100, 500, 800, 1300, 1800]
+        pairs = list(itertools.product(temperatures, pressures))
+        new_data = []
+        for ILs in data_model.dict().keys():
+            print(ILs)
+            my_dict = {}
+
+            ILs_data = data_model.__dict__[ILs]
+            for pair in pairs:
+                temperature, pressure = pair
+                my_dict[f"{ILs}_t_{temperature}_p_{pressure}"] = {
+                    "SR": [x.SR for x in ILs_data if x.T == temperature and x.P == pressure],
+                    "SS": [x.SS for x in ILs_data if x.T == temperature and x.P == pressure],
+                    "cP": [x.cP for x in ILs_data if x.T == temperature and x.P == pressure],
+                }
+
+                self.make_heatmap(my_dict, ILs, temperature, pressure)
+
+            new_data.append(my_dict)
+        return new_data
+
+
 if __name__ == "__main__":
     # read the data json file from the data folder
     data_folder = Path("data/")
@@ -45,36 +94,9 @@ if __name__ == "__main__":
         data = json.load(json_file)
 
     # print(data)
+
     # parse the data into a pydantic model
     data_model = Data(**data)
 
-    HPyBF4_CP = data_model.HPyBF4_CP
-    hpy_bf3_cp_t_74 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 74],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 74]
-    }
-
-    hpy_bf3_cp_t_120 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 120],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 120]
-    }
-
-    hpy_bf3_cp_t_220 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 220],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 220]
-    }
-
-    hpy_bf3_cp_t_310 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 310],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 310]
-    }
-
-    hpy_bf3_cp_t_410 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 410],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 410]
-    }
-
-    hpy_bf3_cp_t_480 = {
-        "SR": [x.SR for x in HPyBF4_CP if x.T == 480],
-        "SS": [x.SS for x in HPyBF4_CP if x.T == 480]
-    }
+    obj = MyWork(data_model)
+    obj.method_1()
